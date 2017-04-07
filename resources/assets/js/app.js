f7eee3b2f6ff6526b40060a18280b0a52d7652e2
Yugo <dedy.yugo.purwanto@gmail.com>
2017-04-07 11:09:43 +0700

    Add pagination component

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,7 +10,10 @@ require('./bootstrap');
 * Next, we will create a fresh Vue application instance and attach it to
 * the page. Then, you may begin adding components to this application
 * or customize the JavaScript scaffolding to fit your unique needs.
-*
+*/
+Vue.component('pagination', require('./components/pagination.vue'));
+
+/**
 * Vue Router
 *
 * @link http://router.vuejs.org/en/installation.html
