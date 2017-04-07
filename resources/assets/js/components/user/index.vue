927881a7bfeb23aa8b8ff0ab9997bd629ee24337
Yugo <dedy.yugo.purwanto@gmail.com>
2017-04-07 11:10:00 +0700

    Add pagination on user index

--- resources/assets/js/components/user/index.vue
+++ resources/assets/js/components/user/index.vue
@@ -1,40 +1,37 @@
 <template>
    <div class="row"> 
-        <div class="btn-floating" id="help-actions">
-           <div class="btn-bg"></div>
-           <router-link :to="{ name: 'user.create' }" class="btn btn-default btn-toggle">
-             <i class="icon fa fa-plus"></i>
-           </router-link>
-         </div>
+      <div class="btn-floating" id="help-actions">
+         <div class="btn-bg"></div>
+         <router-link :to="{ name: 'user.create' }" class="btn btn-default btn-toggle">
+            <i class="icon fa fa-plus"></i>
+         </router-link>
+      </div>
 
-      <div class="col-md-12">
+      <div class="col-xs-12">
          <div class="card">
-            <div class="header">
-               <h4 class="title">User Managements</h4>
-               <p class="category">You can create, edit, and delete users here</p>
+            <div class="card-header">
+               User Management
             </div>
-            <div class="content table-responsive table-full-width">
-
-               <table class="table table-hover table-striped">
+            <div class="card-body no-padding">
+               <table class="datatable table table-striped primary" cellspacing="0" width="100%">
                   <thead>
                      <tr>
                         <th>#</th>
                         <th>Name</th>
-                        <th>Email</th>
-                        <th>Registered</th>
+                        <th>Email Address</th>
+                        <th>Registered at</th>
                         <th>Actions</th>
                      </tr>
                   </thead>
-
                   <tbody>
-                     <tr v-for="(user, index) in users.data">
+                  <tr v-for="(user, index) in users.data">
                         <td>{{ users.from + index }}</td>
                         <td>{{ user.name }}</td>
                         <td>{{ user.email }}</td>
                         <td>{{ user.created_at }}</td>
                         <td>
-                           <router-link :to="{ name: 'user.profile', params: {id: user.id} }" class="btn btn-xs btn-info btn-fill">
-                              <i class="fa fa-user"></i>
+                           <router-link :to="{ name: 'user.profile', params: {id: user.id }}" class="btn btn-xs btn-default">
+                              <i class="fa fa-user fa-fw"></i>
                            </router-link>
                         </td>
                      </tr>
@@ -42,15 +39,7 @@
                </table>
             </div>
          </div>
-
-         <ul class="pagination">
-            <li v-if="users.prev_page_url">
-               <a @click.prevent="paginate(users.prev_page_url)" :href="users.prev_page_url">&laquo; Previous</a>
-            </li>
-            <li v-if="users.next_page_url">
-               <a @click.prevent="paginate(users.next_page_url)" :href="users.next_page_url">Next &raquo;</a>
-            </li>
-         </ul>
+         <pagination :data="users"></pagination>
       </div>
    </div>
 </template>
@@ -66,6 +55,10 @@
 
       mounted() {
          this.paginate(this.url);
+
+         this.$bus.$on('pagination', url => {
+            this.paginate(url);
+         });
       },
 
       methods: {
