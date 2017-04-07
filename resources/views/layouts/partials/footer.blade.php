727bcda8b5655b315dd64c62c1e475caa2783f38
Yugo <dedy.yugo.purwanto@gmail.com>
2017-04-07 10:32:53 +0700

    Separate footer into single file

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="app-footer">
+   <div class="row">
+      <div class="col-xs-12">
+         <div class="footer-copyright" v-cloak>
+         Copyright &copy; 2017 @{{ app.name }}.
+         </div>
+      </div>
+   </div>
+</footer>
