7559dc2cfb5cbb81aac1b5398efc0f724f96e6eb
Yugo <dedy.yugo.purwanto@gmail.com>
2017-04-07 10:34:24 +0700

    Login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,80 +1,64 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Flat Admin V.3 - Free flat-design bootstrap administrator templates</title>
+@extends('layouts.auth')
 
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/vendor.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/admin.css') }}">
-
-  <!-- Theme -->
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/theme/red.css') }}">
-
-</head>
-<body>
-  <div class="app app-default">
-
+@section('content')
 <div class="app-container app-login">
-  <div class="flex-center">
-    <div class="app-header"></div>
-    <div class="app-body">
-      <div class="loader-container text-center">
-          <div class="icon">
-            <div class="sk-folding-cube">
-                <div class="sk-cube1 sk-cube"></div>
-                <div class="sk-cube2 sk-cube"></div>
-                <div class="sk-cube4 sk-cube"></div>
-                <div class="sk-cube3 sk-cube"></div>
-              </div>
+   <div class="flex-center">
+      <div class="app-header"></div>
+      <div class="app-body">
+         <div class="loader-container text-center">
+            <div class="icon">
+               <div class="sk-folding-cube">
+                  <div class="sk-cube1 sk-cube"></div>
+                  <div class="sk-cube2 sk-cube"></div>
+                  <div class="sk-cube4 sk-cube"></div>
+                  <div class="sk-cube3 sk-cube"></div>
+               </div>
             </div>
-          <div class="title">Logging in...</div>
-      </div>
-      <div class="app-block">
-      <div class="app-form">
-        <div class="form-header">
-          <div class="app-brand"><span class="highlight">{{ config('app.name') }}</span> Admin</div>
-        </div>
-        <form action="/" method="POST">
-            <div class="input-group">
-              <span class="input-group-addon" id="basic-addon1">
-                <i class="fa fa-user" aria-hidden="true"></i></span>
-              <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
-            </div>
-            <div class="input-group">
-              <span class="input-group-addon" id="basic-addon2">
-                <i class="fa fa-key" aria-hidden="true"></i></span>
-              <input type="text" class="form-control" placeholder="Password" aria-describedby="basic-addon2">
-            </div>
-            <div class="text-center">
-                <input type="submit" class="btn btn-success btn-submit" value="Login">
-            </div>
-        </form>
+         </div>
+         <div class="app-block">
+            <div class="app-form">
+               <div class="form-header">
+                  <div class="app-brand"><span class="highlight">{{ config('app.name') }}</span> Admin</div>
+               </div>
+               <form action="{{ route('login') }}" method="POST">
+                  {{ csrf_field() }}
+                  {{ method_field('post') }}
 
-        <div class="form-line">
-          <div class="title">OR</div>
-        </div>
-        <div class="form-footer">
-          <button type="button" class="btn btn-default btn-sm btn-social __facebook">
-            <div class="info">
-              <i class="icon fa fa-facebook-official" aria-hidden="true"></i>
-              <span class="title">Login with Facebook</span>
+                  <div class="input-group">
+                     <span class="input-group-addon" id="basic-addon1">
+                        <i class="fa fa-envelope" aria-hidden="true"></i></span>
+                        <input type="text" name="email" class="form-control" placeholder="Email Address" aria-describedby="basic-addon1">
+                  </div>
+
+                  <div class="input-group">
+                     <span class="input-group-addon" id="basic-addon2">
+                        <i class="fa fa-key" aria-hidden="true"></i></span>
+                        <input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon2">
+                  </div>
+
+                  <div class="text-center">
+                     <button type="submit" class="btn btn-success btn-submit">
+                        Login
+                     </button>
+                  </div>
+               </form>
+
+               <div class="form-line">
+                  <div class="title">OR</div>
+               </div>
+               <div class="form-footer">
+                  <button disabled="true" type="button" class="btn btn-default btn-sm btn-social __facebook">
+                     <div class="info">
+                        <i class="icon fa fa-facebook-official" aria-hidden="true"></i>
+                        <span class="title">Login with Facebook</span>
+                     </div>
+                  </button>
+               </div>
             </div>
-          </button>
-        </div>
+         </div>
       </div>
+      <div class="app-footer">
       </div>
-    </div>
-    <div class="app-footer">
-    </div>
-  </div>
+   </div>
 </div>
-
-  </div>
-
-  <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('js/vendor.js') }}"></script>
-  <script type="text/javascript" src="{{ asset('js/admin.js') }}"></script>
-
-</body>
-</html>
+@stop
